21b1aaf8ec36aad855644f69752e43faeed4f6a3
qihua <zhongqihua@gmail.com>
2020-11-15 14:50:10 -0500

    demands.py
    - set default demand size 100
    - made high parameter inclusive in uniform demand type
    - Set demand to be non negative (in normal distribution demand type)

--- demands.py
+++ demands.py
@@ -3,14 +3,15 @@ import numpy as np
 
 class DemandGenerator:
 
-    def __init__(self, demands_pattern='classic_beer_game', size=None, low=None, high=None, 
+    def __init__(self, demands_pattern='classic_beer_game', size=100, low=None, high=None,
                  mean=None, sd=None):
         """
         Args:
             demands_pattern: can be a string ('normal', 'uniform', 'classic_beer_game') to specify a stochastic demand
                 pattern or a list of numbers to specify deterministic demands.
-            low: must be provided when uniform pattern is specified
-            high: must be provided when uniform pattern is specified
+            size: number of periods in an episode
+            low: inclusive, must be provided when uniform pattern is specified
+            high: inclusive, must be provided when uniform pattern is specified
             mean: mean of a normal distribution, must be provided when normal pattern is specified
             sd: standard deviation of a normal distribution, must be provided when normal pattern is specified
         """
@@ -29,15 +30,15 @@ class DemandGenerator:
         elif self.demands_pattern == 'uniform':
             if (self.low is None) or (self.high is None):
                 raise ValueError('"low" and "high" need to be provided when uniform pattern is specified')
-            self.demands = np.random.randint(self.low, self.high, self.size)
+            self.demands = np.random.randint(self.low, self.high+1, self.size)
 
         elif self.demands_pattern == 'normal':
-            self.demands = (self.mean + self.sd * np.random.randn(self.size)).astype(int)
+            self.demands = np.maximum(self.mean + self.sd * np.random.randn(self.size), 0).astype(int)
             if (self.mean is None) or (self.sd is None):
                 raise ValueError('"mean" and "sd" need to be provided when normal pattern is specified')
 
         elif self.demands_pattern == 'classic_beer_game':
-            self.demands = np.array([4] * 4 + [8] * 31)
+            self.demands = np.array([4] * 4 + [8] * (self.size - 4))
 
         else:
             raise ValueError("Demand pattern not recognized")
